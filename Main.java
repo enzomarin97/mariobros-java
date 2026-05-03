f4765ef57167da270bd5037c4c549e2dbc236d05
enzomarin97 <enzomarin.me@gmail.com>
2026-05-03 12:23:42 -0300

    chore: add .gitignore para ignorar archivos compilados .class | fix: Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
-public static void main(String[] args) {
-    Movimientos mario = new Mario("rojo", "mario");
-    Movimientos luigi = new Mario("verde", "mario");
-
-    mario.moverArriba();
-    luigi.moverDerecha();
+public class Main {
+    public static void main(String[] args) {
+        Movimientos mario = new Mario("rojo", "mario");
+        Movimientos luigi = new Mario("verde", "mario");
+    
+        mario.moverArriba();
+        luigi.moverDerecha();
+    }
 }
